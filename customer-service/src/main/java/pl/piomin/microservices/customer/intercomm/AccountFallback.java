276ffc37168c0c630fff209949775fb446b44268
Laijin Lu (ThoughtWorks) <ljlu@thoughtworks.com>
2020-06-27 18:00:45 +0800

    fix eureka for java 11

--- customer-service/src/main/java/pl/piomin/microservices/customer/intercomm/AccountFallback.java
+++ customer-service/src/main/java/pl/piomin/microservices/customer/intercomm/AccountFallback.java
@@ -2,18 +2,20 @@ package pl.piomin.microservices.customer.intercomm;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.logging.Logger;
 import org.springframework.stereotype.Component;
-
 import pl.piomin.microservices.customer.model.Account;
 
 @Component
 public class AccountFallback implements AccountClient {
 
-	@Override
-	public List<Account> getAccounts(Integer customerId) {
-		List<Account> acc = new ArrayList<Account>();
-		return acc;
-	}
-	
+    protected Logger logger = Logger.getLogger(AccountFallback.class.getName());
+
+    @Override
+    public List<Account> getAccounts(Integer customerId) {
+        logger.info(String.format("Account.getAccounts(%s) fallback", customerId));
+        List<Account> acc = new ArrayList<Account>();
+        return acc;
+    }
+
 }
--- customer-service/src/test/java/pl/piomin/microservices/customer/ApiTest.java
+++ customer-service/src/test/java/pl/piomin/microservices/customer/ApiTest.java
@@ -28,5 +28,5 @@ public class ApiTest {
 			Thread.sleep(500);
 		}
 	}
-	
+
 }
